e0a9b0e363b3d3552da63ea1bb1354c701548442
Ricky Yip <ricky.yip@ascotchang.com>
2018-05-08 15:19:12 +0800

    add lcok to get-sound -link api

--- src/PartyGames.Web/Controllers/McController.cs
+++ src/PartyGames.Web/Controllers/McController.cs
@@ -98,6 +98,7 @@ namespace PartyGames.Web.Controllers
 
         #region web API
 
+        private static object _rLock = new object();
 
         [Route("get-sound-link"), HttpGet]
         public ActionResult GetSoundLinkFromCambridge(string word)
@@ -105,85 +106,88 @@ namespace PartyGames.Web.Controllers
             if (string.IsNullOrWhiteSpace(word))
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
 
-
-            word = word.ToLower().Trim();
-            //var url = $"https://dictionary.cambridge.org/dictionary/english/{word}";
-            var url = $"https://dictionary.cambridge.org/search/english/direct/?q={word}";
-            try
+            lock (_rLock)
             {
-                var request = WebRequest.Create(url) as HttpWebRequest;
-
-                request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
-                request.Host = "dictionary.cambridge.org";
-                request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36";
-
-                request.Headers.Add(HttpRequestHeader.AcceptLanguage, "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,ko;q=0.6,zh-CN;q=0.5,ja;q=0.4");
-                request.Headers.Add(HttpRequestHeader.CacheControl, "no-cache");
-                request.Headers.Add(HttpRequestHeader.Pragma, "no-cache");
-                request.Headers.Add(HttpRequestHeader.Upgrade, "1");
-
-                //ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
-                ServicePointManager.Expect100Continue = true;
-                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
-                ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
-
-                var html = string.Empty;
-
-                using (var response = request.GetResponse() as HttpWebResponse)
+                word = word.ToLower().Trim();
+                //var url = $"https://dictionary.cambridge.org/dictionary/english/{word}";
+                var url = $"https://dictionary.cambridge.org/search/english/direct/?q={word}";
+                try
                 {
-                    var sb = new StringBuilder();
-                    var buf = new byte[8192];
-                    using (var resStream = response.GetResponseStream())
+                    var request = WebRequest.Create(url) as HttpWebRequest;
+
+                    request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
+                    request.Host = "dictionary.cambridge.org";
+                    request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36";
+
+                    request.Headers.Add(HttpRequestHeader.AcceptLanguage, "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,ko;q=0.6,zh-CN;q=0.5,ja;q=0.4");
+                    request.Headers.Add(HttpRequestHeader.CacheControl, "no-cache");
+                    request.Headers.Add(HttpRequestHeader.Pragma, "no-cache");
+                    request.Headers.Add(HttpRequestHeader.Upgrade, "1");
+
+                    //ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+                    ServicePointManager.Expect100Continue = true;
+                    ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+                    ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+
+                    var html = string.Empty;
+
+                    using (var response = request.GetResponse() as HttpWebResponse)
                     {
-                        int count = 0;
-
-                        do
+                        var sb = new StringBuilder();
+                        var buf = new byte[8192];
+                        using (var resStream = response.GetResponseStream())
                         {
-                            count = resStream.Read(buf, 0, buf.Length);
-                            if (count != 0)
+                            int count = 0;
+
+                            do
                             {
-                                sb.Append(Encoding.UTF8.GetString(buf, 0, count)); // just hardcoding UTF8 here
-                            }
-                        } while (count > 0);
+                                count = resStream.Read(buf, 0, buf.Length);
+                                if (count != 0)
+                                {
+                                    sb.Append(Encoding.UTF8.GetString(buf, 0, count)); // just hardcoding UTF8 here
+                                }
+                            } while (count > 0);
 
-                        html = sb.ToString();
+                            html = sb.ToString();
+                        }
                     }
+
+                    var doc = new HtmlDocument();
+                    doc.LoadHtml(html);
+                    var node = doc.DocumentNode.Descendants(0).FirstOrDefault(c => c.HasClass("audio_play_button") && c.HasClass("uk"));
+
+
+                    if (node == null)
+                        node = doc.DocumentNode.Descendants(0).FirstOrDefault(c => c.HasClass("audio_play_button"));
+
+                    if (node == null)
+                        throw new Exception("Cannot find docuemnt node with class 'audio_play_button' & 'uk/us'");
+
+
+
+                    return Json(new
+                    {
+                        Success = true,
+                        Message = "Success",
+                        //Country = node.HasClass("uk") ? "uk" : "us",
+                        Country = node.ParentNode.Descendants("span").FirstOrDefault(c => c.HasClass("region"))?.InnerHtml,
+                        Mp3 = node.GetAttributeValue("data-src-mp3", null),
+                        Ogg = node.GetAttributeValue("data-src-ogg", null),
+                        InquiryTime = DateTime.Now.ToString("O")
+                    }, JsonRequestBehavior.AllowGet);
+
                 }
-
-                var doc = new HtmlDocument();
-                doc.LoadHtml(html);
-                var node = doc.DocumentNode.Descendants(0).FirstOrDefault(c => c.HasClass("audio_play_button") && c.HasClass("uk"));
-
-
-                if (node == null)
-                    node = doc.DocumentNode.Descendants(0).FirstOrDefault(c => c.HasClass("audio_play_button"));
-
-                if (node == null)
-                    throw new Exception("Cannot find docuemnt node with class 'audio_play_button' & 'uk/us'");
-
-
-
-                return Json(new
+                catch (Exception message)
                 {
-                    Success = true,
-                    Message = "Success",
-                    //Country = node.HasClass("uk") ? "uk" : "us",
-                    Country = node.ParentNode.Descendants("span").FirstOrDefault(c => c.HasClass("region"))?.InnerHtml,
-                    Mp3 = node.GetAttributeValue("data-src-mp3", null),
-                    Ogg = node.GetAttributeValue("data-src-ogg", null),
-                    InquiryTime = DateTime.Now.ToString("O")
-                }, JsonRequestBehavior.AllowGet);
+                    return Json(new
+                    {
+                        Success = false,
+                        Message = message.Message,
+                        InquiryTime = DateTime.Now.ToString("O")
+                    }, JsonRequestBehavior.AllowGet);
+                }
+            }
 
-            }
-            catch (Exception message)
-            {
-                return Json(new
-                {
-                    Success = false,
-                    Message = message.Message,
-                    InquiryTime = DateTime.Now.ToString("O")
-                }, JsonRequestBehavior.AllowGet);
-            }
         }
 
         #endregion
